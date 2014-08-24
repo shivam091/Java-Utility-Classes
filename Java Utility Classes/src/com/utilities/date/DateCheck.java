5c885a9cc4f9eab0a19f3284ca06ba9bf79df4e9
shivam <shivam@SHIVAM-PC>
2014-08-24 22:46:01 +0530

    utility classes for operations on date.

---
+++ Java Utility Classes/src/com/utilities/date/DateCheck.java
@@ -0,0 +1,43 @@
+package com.utilities.date;
+
+import java.util.Date;
+
+/**
+ * utility class for operations on dates.
+ * @author shivam
+ *
+ */
+public class DateCheck {
+
+	/**
+	 * method to check whether the date is before the specified date.
+	 * @param currentDate
+	 * 			current date.
+	 * @param previousDate
+	 * 			date to be checked.
+	 * @return true, if successful.
+	 */
+	public static Boolean checkDateForBefore(Date currentDate, Date previousDate){
+        if(previousDate.before(currentDate)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+	
+	/**
+	 * method to check whether the date is after the specified date.
+	 * @param currentDate
+	 * 			current date.
+	 * @param nextDate
+	 * 			date to be checked.
+	 * @return true, if successful.
+	 */
+	public static Boolean main(Date currentDate, Date nextDate){
+        if(nextDate.after(currentDate)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
